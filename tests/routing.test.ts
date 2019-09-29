e0f4f8ff1f499166dbbf07ec3e4a6ccd2f1c78d2
Konstantin Isaev <kote.isaev@sakartvelosoft.com>
2019-09-29 15:14:49 +0400

    Added router for API test

--- tests/routing.test.ts
+++ tests/routing.test.ts
@@ -1,6 +1,8 @@
 import {expect} from 'chai';
 import {APICompiledRoute, APIRouter, makeUrlTemplate} from "../src/routing";
 import {APIRequest, defineAPIStructure, HttpVerb} from "../src";
+import {APIResponder} from "../src/pipeline";
+import {APIResultAction} from "../src/api-interfaces";
 
 describe('Test routing infrastructure', () => {
     it('Test url templates', () => {
@@ -22,8 +24,7 @@ describe('Test routing infrastructure', () => {
         });
     });
 
-    it('Test API router with multiple routes', () => {
-        let router = new APIRouter();
+    function fillAPIRouterWithTestStructure(router: APIRouter) {
         let structure = defineAPIStructure({
             version: '1.0.0',
             name: 'test api',
@@ -55,16 +56,42 @@ describe('Test routing infrastructure', () => {
                     return res.json({
                         message: `processed ${req.method} to ${req.url} ${req.matchedRoute.controller}.${req.matchedRoute.action}`,
                         route: req.matchedRoute.name,
-                        controller: req.matchedRoute.apiRoute.controller,
+                        controller: req.matchedRoute.controller,
+                        action: req.matchedRoute.action,
 
                     })
                 })
             }
         }
+    }
+
+    it('Test API router routes picking', () => {
+        let router = new APIRouter();
+        fillAPIRouterWithTestStructure(router);
         let routePickResult = router.tryPickRoute(HttpVerb.POST, '/auth/sign-in');
         expect(routePickResult).is.not.equals(null);
         expect(routePickResult.route.name).equals('Authentication/Sign In');
         expect(routePickResult.route.controller).equals('auth');
         expect(routePickResult.route.action).equals('signIn');
-    })
+    });
+
+    it('Test API router request flow', (done) => {
+        let router = new APIRouter();
+        fillAPIRouterWithTestStructure(router);
+        let routePickResult = router.tryPickRoute(HttpVerb.POST, '/auth/sign-in');
+        expect(routePickResult).is.not.equals(null);
+        let responder = new APIResponder();
+        let request = new APIRequest(HttpVerb.POST, '/auth/sign-in', routePickResult.route, {}, null, {}, 'test1');
+        router.processRequest(request, responder).then(() => {
+            expect(responder.action).equal(APIResultAction.SendJSON);
+            expect(responder.body).deep.equal({
+                message: `processed ${request.method} to ${request.url} ${request.matchedRoute.controller}.${request.matchedRoute.action}`,
+                route: request.matchedRoute.name,
+                controller: request.matchedRoute.controller,
+                action: request.matchedRoute.action,
+            });
+            done();
+        });
+    });
+
 });
